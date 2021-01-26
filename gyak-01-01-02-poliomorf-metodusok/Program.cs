0618e81e704ab0d23120a6973680ebe6952f3a23
Csaba Gyuris <gyuris.csaba@vasvari.org>
2021-01-26 16:54:58 +0100

    Termek proejkt feladattal.

---
+++ gyak-01-01-02-poliomorf-metodusok/Program.cs
@@ -0,0 +1,24 @@
+﻿// 2.	TermekProjekt
+// Készítsen osztályokat különböző termékekből (pl. hűtő, sütő, kenyérpirító,…).
+// A termékek egy elárusítással foglalkozó cég kezeli.
+// A cég alkalmazottja a különböző termékeket tudja a raktárba bevételezni és a raktárból kiadni.
+// Írja meg ennek megfelelően a szükséges metódusokat a polimorfizmus elvét figyelembe véve.
+// Mindegyik metódus egy megfelelő szöveget írjon ki a képernyőre
+// (pl. „A munkatárs bevételezte a sütőt.” vagy „A munkatárs kiadta a kenyérpirítót.”!
+// A cég József nevű dolgozója bevételezzen egy terméket!
+// Ezután a cég Éva nevű dolgozója bevételezzen egy terméket!
+// Ezután Éva adja ki a bevételezett terméket, majd József is adja ki a bevételezett terméket!
+// Az elkészült osztály dokumentálja UML diagrammal!
+
+using System;
+
+namespace gyak_01_01_02_poliomorf_metodusok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

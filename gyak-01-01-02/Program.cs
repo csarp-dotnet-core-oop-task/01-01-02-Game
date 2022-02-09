9f1816d879d95363a1cffc063a90fa9bda6869be
Gyuris Csaba <gycsaba72@gmail.com>
2022-02-09 17:35:05 +0100

    Update Program.cs

--- gyak-01-01-02/Program.cs
+++ gyak-01-01-02/Program.cs
@@ -1,12 +1,12 @@
 ﻿/*
-Egy játékban kutyák és macskák szerepelnek! Ezek az áttatok a játékban futnak és állnak!
-Készítsen kutya (Dog) és macska (Cat) osztályt!
-Készíts el a polimorf metódusokat Run és Stand néven! 
-Mindegyik metódus egy megfelelő szöveget írjon ki a képernyőre (pl. „A kugya szalad.” vagy „A macska áll.”)
-A feladat megoldása megfelelő, ha a tesztek lefutnak!
-Példányosítsa Tom macskát és Spike kugyát. 
-Szimuláljon egy versenyt közöttük, ahol ez egyik előbb indul, a másik előbb érkezik meg!
-Az elkészült osztály dokumentálja UML diagrammal!
+Egy játékban kutyák és macskák szerepelnek! Ezek az állatok a játékban futnak és állnak! 
+Készítsen kutya (Dog) és macska (Cat) osztályt! Készíts el a polimorf metódusokat Run és Stand néven! 
+Mindegyik metódus egy megfelelő szöveget írjon ki a képernyőre (pl. „A kutya szalad.” vagy „A macska áll.”
+
+A kiírások után új sor karakter kerül a konzolra! A feladat megoldása megfelelő, ha a tesztek lefutnak!
+
+Példányosítsa Tom macskát és Spike kugyát. Szimuláljon egy versenyt közöttük, ahol ez egyik előbb indul,
+a másik előbb érkezik meg! Az elkészült osztály dokumentálja UML diagrammal!
 */
 
 using System;
